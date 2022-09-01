383a03581b6f6ffccadebdb776252490f24ec56c
kennethachan <kennethandrechan@gmail.com>
2022-09-01 12:05:29 -0400

    initial middleware and auth routes set up

--- routes/ProfileRouter.js
+++ routes/ProfileRouter.js
@@ -1,5 +1,13 @@
 const Router = require("express").Router()
 const controller = require("../controllers/ProfileController")
+const middleware = require("../middleware")
+
+Router.get(
+  "/session",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CheckSession
+)
 
 Router.get("/get-profiles", controller.GetPosts)
 Router.get("/get-profile", controller.GetPostById)
