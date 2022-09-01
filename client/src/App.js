e9634d17e25dcf0d01fd906b3144530891cb8ec5
kennethachan <kennethandrechan@gmail.com>
2022-09-01 17:09:29 -0400

    react app initialized

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg"
+import "./App.css"
+import { Route, Routes } from "react-router"
+import { useState, useEffect } from "react"
+
+function App() {
+  return <div className="App"></div>
+}
+
+export default App
