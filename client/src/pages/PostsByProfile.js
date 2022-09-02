3feaec37dda7c5d077cf39abbc67c961acf619e5
kennethachan <kennethandrechan@gmail.com>
2022-09-02 18:46:50 -0400

    spots by borough is now fully functional

--- client/src/pages/PostsByProfile.js
+++ client/src/pages/PostsByProfile.js
@@ -6,11 +6,13 @@ import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom"
 import axios from "axios"
 import PostsByProfileCard from "../components/PostsByProfileCard"
+import "./PostsByProfile.css"
 
 const URL = "http://localhost:3001"
 
 function PostsByProfile(props) {
   const [spots, setSpots] = useState([])
+  const [profile, setProfile] = useState([])
 
   let { profileId } = useParams()
   let navigate = useNavigate()
@@ -21,20 +23,24 @@ function PostsByProfile(props) {
 
   const getSpotsByProfile = async () => {
     const res = await axios.get(`${URL}/api/profiles/${profileId}`)
+    console.log(res.data)
     console.log(res.data.Posts)
+    setProfile(res.data)
     setSpots(res.data.Posts)
   }
   return (
     <div>
-      <div className="">
-        <p className="back-to-boroughs">
-          <Link className="back-to-boroughs" to="/spots-by-borough">
-            {" "}
-            Back To Boroughs
-          </Link>
-        </p>
+      <p className="back-to-boroughs">
+        <Link className="back-to-boroughs" to="/spots-by-borough">
+          {" "}
+          Back To Boroughs
+        </Link>
+      </p>
+      <div className="user-info">
+        <img className="profile-img" src={profile.image}></img>
+        <h2 className="userName">{profile.userName}'s Finds!</h2>
       </div>
-      <div className="">
+      <div>
         {spots.map((spot) => (
           <PostsByProfileCard
             key={spot.id}
