e16594db9c597c464acaa5772bd9fa0c4bc79c70
kennethachan <kennethandrechan@gmail.com>
2025-03-10 09:36:01 -0400

    updated Feed GET request

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -15,7 +15,7 @@ function Feed(props) {
   }, [])
 
   const getPosts = async () => {
-    const res = await axios.get(`${URL}`)
+    const res = await axios.get(`${URL}/api/posts/get-posts`)
     console.log(res.data)
     setPosts(res.data)
   }
